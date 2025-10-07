e669248b65682e8299091eddc9e569c065163599
Anotida Maunganidze <ax1tfg@gmail.com>
2025-10-07 23:32:48 +0200

    KNN notebook

---
+++ SupervisedLearning/kNN.py
@@ -0,0 +1,5 @@
+#this is going to be an implementation of k-Nearest neighbour
+#this is a lazy algorithm that stores the data and only trains when query is passed
+#it checks the nearest neighbours of a new data point using the distance to determine points to take
+#yes for now I have just gone through the tutorial from tutorials point but I intend on finding my own dataset and try it from scratch
+#can be used for both classification and regression
